64f2fd6c11c0cd679297eac60b1f355f29e39ea5
Nate Worley <immythz@gmail.com>
2021-07-20 14:28:51 -0500

    modified nav links on all html pages and added CSS. Added comments for userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,10 +6,12 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/index.html'))
 })
 
+// Renders exercise page
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/exercise.html'))
 })
 
+// Renders stats page
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/stats.html'))
 })
