32150bfb2893d7ac9281c808969e0ed0d5361c8d
Nate Worley <immythz@gmail.com>
2021-07-20 14:22:46 -0500

    added routes for exercise and stats page. Modified script and link tags to wire js and css together

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/index.html'))
 })
 
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/exercise.html'))
+})
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/stats.html'))
+})
+
 module.exports = router;
