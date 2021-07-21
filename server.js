08993de47ae3097f650c496b34cd564514451f23
Nate Worley <immythz@gmail.com>
2021-07-21 13:58:38 -0500

    updated mongodb connection to connect to Heroku

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false }
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 // Start the API server
